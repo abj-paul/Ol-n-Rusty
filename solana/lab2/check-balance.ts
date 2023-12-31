9c14b9062142e0b257b89a58e674f70c4d7509fb
Abhijit Paul <bsse1201@iit.du.ac.bd>
2023-12-31 12:34:42 +0600

    completed challenge on check balance

--- solana/lab2/check-balance.ts
+++ solana/lab2/check-balance.ts
@@ -3,11 +3,20 @@ import { Connection, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
 import { getKeypairFromEnvironment } from "@solana-developers/node-helpers";
 
 const keypair = getKeypairFromEnvironment("SECRET_KEY");
-const publicKey = new PublicKey("31ZdXAvhRQyzLC2L97PC6Lnf2yWgHhQUKKYoUo9MLQF5");
-const connection = new Connection("https://api.devnet.solana.com", "confirmed");
-const balance = await connection.getBalance(publicKey);
-const balanceInSOL = balance / LAMPORTS_PER_SOL;
+try {
+  const publicKey = new PublicKey("to1yVXiNRMVVgS854Yjj3xB51MTorFrCMz7N8cirbK");
 
-console.log(
-  `💰 Finished! The balance for the wallet at address ${publicKey} is ${balance} Lamports, ${balanceInSOL} Sol!`
-);
+  const connection = new Connection("https://api.mainnet-beta.solana.com", "confirmed");
+  const balance = await connection.getBalance(publicKey);
+  const balanceInSOL = balance / LAMPORTS_PER_SOL;
+
+  console.log(
+    `💰 Finished! The balance for the wallet at address ${publicKey} is ${balance} Lamports, ${balanceInSOL} Sol!`
+  );
+} catch(e){
+  console.log(`${e.message}!`);
+}
+
+
+
+
